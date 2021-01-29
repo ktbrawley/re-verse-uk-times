9ec7402592a6332b70dfa39de8ba753ecbc48d2b
ktbrawley <69354486+ktbrawley@users.noreply.github.com>
2021-01-29 12:51:34 +0000

    scrap uk beta times from re-verse site

---
+++ re-verse.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+reverse_uk_url = "https://www.residentevil.com/reverse/uk/topic/cbt/"
+page = requests.get(reverse_uk_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+timeEls = soup.find_all(class_="mb5 ind1")[:4]
+
+for timeEl in timeEls:
+    print(timeEl.text)
